78050a8141e709badfe0ae019bd6c1a11113a4a6
Nikita Popov <losnikitos@gmail.com>
2026-06-04 17:01:26 +0100

    Bundles in yml

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @bundle_rows = YAML.safe_load_file(
+      Rails.root.join("db/fixtures/bundle_rows.yml"),
+      symbolize_names: true
+    )
   end
 end
