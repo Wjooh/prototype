9197131bf1445f5f7a9333966f243beb3c66a984
Nikita Popov <losnikitos@gmail.com>
2026-06-04 16:49:35 +0100

    Header

---
+++ config/initializers/heroicons.rb
@@ -0,0 +1,10 @@
+Heroicons.configure do |config|
+  config.variant = :outline
+
+  config.default_class = {
+    solid: "h-5 w-5 shrink-0",
+    outline: "h-5 w-5 shrink-0",
+    mini: "h-4 w-4 shrink-0",
+    micro: "h-3 w-3 shrink-0"
+  }
+end
